56016231ccff58835f28dea561d13ae30bb00cb4
dzl-i <denzel.iskandar@gmail.com>
2024-06-29 16:39:29 +1000

    feat: profile picture for users

---
+++ server/prisma/migrations/20240629063820_4_profile_picture/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "profilePicture" TEXT NOT NULL DEFAULT 'https://www.its.ac.id/international/wp-content/uploads/sites/66/2020/02/blank-profile-picture-973460_1280.jpg';
